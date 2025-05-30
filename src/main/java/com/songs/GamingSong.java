a714e6514ad47ba1258e35376a414cbca92e3193
Cristian Daniel <hernandezcardona35@gmail.com>
2025-05-30 20:57:15 +0000

    🔺 A small change of the files. 🔺

---
+++ src/main/java/com/songs/GamingSong.java
@@ -0,0 +1,5 @@
+package com.songs;
+
+public class GamingSong {
+
+}
--- src/main/java/com/songs/gamingsongs.java
+++
@@ -1,7 +0,0 @@
-package com.songs;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class gamingsongs extends JavaPlugin {
-
-}
