5adb3ad6975d5095dddc943028a90b2750c9f9e6
Cristian Daniel <hernandezcardona35@gmail.com>
2025-05-31 03:43:14 +0000

    pom.xml versión 1-1beta

--- src/main/java/com/songs/GamingSong.java
+++ src/main/java/com/songs/GamingSong.java
@@ -1,5 +1,25 @@
 package com.songs;
 
-public class GamingSong {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class GamingSong extends JavaPlugin {
+
+    // Activation code of the plugin
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', "&a==================================")
+        );
+    }
+
+    // Desactivation code of the plugin
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', ("&c==================================")
+            )
+        );
+    }
 }
