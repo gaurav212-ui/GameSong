e4aa561135beff081b4955d99742ee2d3179f3d7
Cristian Daniel <hernandezcardona35@gmail.com>
2025-06-01 05:22:59 +0000

    🔆 version beta 🔆

--- src/main/java/com/songs/GamingSong.java
+++ src/main/java/com/songs/GamingSong.java
@@ -5,13 +5,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GamingSong extends JavaPlugin {
-
+    
     // Activation code of the plugin
     @Override
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage(
             ChatColor.translateAlternateColorCodes('&', "&a==================================")
         );
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', "&aGamingSong plugin has been enabled!")
+        );
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', ("==================================="))
+        );
+
     }
 
     // Desactivation code of the plugin
@@ -20,6 +27,13 @@ public class GamingSong extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage(
             ChatColor.translateAlternateColorCodes('&', ("&c==================================")
             )
+        );
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', ("&cGamingSong plugin has disabled!")
+            )
+        );
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', ("===================================="))
         );
     }
 }
